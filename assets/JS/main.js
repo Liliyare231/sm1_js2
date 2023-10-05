76569b37ed15f40c7259eac9c8f994a5a74d8781
Liliyare231 <fazlievaliliya0@gmail.com>
2023-10-05 11:11:31 +0300

    sm1_js2

---
+++ assets/JS/main.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menuBtn')
+const menuList = document.querySelector('.menuList')
+
+menuBtn.addEventListener('click', ()=>{
+    menuList.classList.toggle('show')
+})
+
+const menuBtn2 = document.querySelector('.menuBtn2')
+const menuList2 = document.querySelector('.menuList2')
+
+menuBtn2.addEventListener('click', ()=>{
+    menuList2.classList.toggle('show')
+})
+
+const menuBtn3 = document.querySelector('.menuBtn3')
+const menuList3 = document.querySelector('.menuList3')
+
+menuBtn3.addEventListener('click', ()=>{
+    menuList3.classList.toggle('show')
+})
